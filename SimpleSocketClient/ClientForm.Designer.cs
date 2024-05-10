ef4a677f04422e8066c2e0f02eb6f8a4fc0051f6
Admin <Admin@LAPTOP-Q6I9MM59>
2024-05-11 00:53:30 +0300

    Добавьте файлы проекта.

---
+++ SimpleSocketClient/ClientForm.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace SimpleSocketClient
+{
+    partial class ClientForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.richTextBoxLog = new System.Windows.Forms.RichTextBox();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.textBoxServerIP = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(618, 18);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(149, 77);
+            this.buttonConnect.TabIndex = 0;
+            this.buttonConnect.Text = "Подключиться к серверу";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(4, 74);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(157, 77);
+            this.buttonSend.TabIndex = 1;
+            this.buttonSend.Text = "Отправить";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // richTextBoxLog
+            // 
+            this.richTextBoxLog.Location = new System.Drawing.Point(21, 209);
+            this.richTextBoxLog.Name = "richTextBoxLog";
+            this.richTextBoxLog.Size = new System.Drawing.Size(746, 229);
+            this.richTextBoxLog.TabIndex = 2;
+            this.richTextBoxLog.Text = "";
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(155, 167);
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(228, 22);
+            this.textBoxMessage.TabIndex = 3;
+            // 
+            // textBoxServerIP
+            // 
+            this.textBoxServerIP.Location = new System.Drawing.Point(667, 129);
+            this.textBoxServerIP.Name = "textBoxServerIP";
+            this.textBoxServerIP.Size = new System.Drawing.Size(100, 22);
+            this.textBoxServerIP.TabIndex = 4;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(12, 37);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 22);
+            this.textBoxName.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Введите имя";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Введите сообщение";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(463, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(198, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Введите IP сервера (127.0.0.1)";
+            // 
+            // ClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.textBoxServerIP);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.richTextBoxLog);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.buttonConnect);
+            this.Name = "ClientForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.ClientForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.RichTextBox richTextBoxLog;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.TextBox textBoxServerIP;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SimpleSocketClient/ClientForm.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleSocketClient
+{
+    public partial class ClientForm : Form
+    {
+        private TcpClient client;
+        private NetworkStream clientStream;
+        private Thread clientThread;
+        private bool isConnected = false;
+
+        public ClientForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ClientForm_Load(object sender, EventArgs e)
+        {
+            //127.0.0.1
+        }
+
+
+        // Метод для подключения к серверу
+        private void ConnectToServer()
+        {
+            try
+            {
+                client = new TcpClient();
+                client.Connect(textBoxServerIP.Text, 8888);
+                clientStream = client.GetStream();
+                clientThread = new Thread(new ThreadStart(ReceiveMessages));
+                clientThread.Start();
+                isConnected = true;
+
+                // Отправка запроса на получение истории сообщений
+                SendMessageToServer("GetHistory \n");
+            }
+            catch (Exception ex)
+            {
+                Log("Error connecting to server: " + ex.Message);
+            }
+        }
+
+        // Метод для отправки сообщения серверу
+        private void SendMessageToServer(string message)
+        {
+            try
+            {
+                byte[] buffer = Encoding.ASCII.GetBytes(message);
+                clientStream.Write(buffer, 0, buffer.Length);
+                clientStream.Flush();
+            }
+            catch (Exception ex)
+            {
+                Log("Error sending message to server: " + ex.Message);
+            }
+        }
+
+
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            if (!isConnected)
+            {
+                ConnectToServer();
+            }
+        }
+
+
+        private void ReceiveMessages()
+        {
+            byte[] message = new byte[4096];
+            int bytesRead;
+            try
+            {
+                while (isConnected && (bytesRead = clientStream.Read(message, 0, 4096)) > 0)
+                {
+                    string receivedMessage = Encoding.ASCII.GetString(message, 0, bytesRead);
+                    if (receivedMessage.StartsWith("History:"))
+                    {
+                        // Обновление richTextBoxLog с полученной историей сообщений
+                        string history = receivedMessage.Substring(8); // Удаление "History:" из строки
+                        UpdateRichTextBox(history);
+                    }
+                    else
+                    {
+                        Log(receivedMessage);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log("Error receiving message from server: " + ex.Message);
+                Disconnect();
+            }
+        }
+
+        // Метод для обновления richTextBoxLog с полученной историей сообщений
+        private void UpdateRichTextBox(string history)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<string>(UpdateRichTextBox), history);
+                return;
+            }
+            richTextBoxLog.AppendText(history + Environment.NewLine);
+        }
+
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (isConnected)
+                {
+                    string message = $"{textBoxName.Text}: {textBoxMessage.Text}";
+                    byte[] buffer = Encoding.ASCII.GetBytes(message);
+                    clientStream.Write(buffer, 0, buffer.Length);
+                    clientStream.Flush();
+                    Log(message);
+                }
+                else
+                {
+                    Log("Not connected to server.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Log("Error sending message: " + ex.Message);
+                Disconnect();
+            }
+        }
+
+        private void Disconnect()
+        {
+            if (isConnected)
+            {
+                isConnected = false;
+                clientStream.Close();
+                client.Close();
+                Log("Disconnected from server.");
+            }
+        }
+
+
+        private void Log(string message)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<string>(Log), message);
+                return;
+            }
+            richTextBoxLog.AppendText(message + Environment.NewLine);
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            Disconnect();
+        }
+    }
+}
